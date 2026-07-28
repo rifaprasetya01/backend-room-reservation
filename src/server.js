f31bc2fafa27d2b1b8fa1b4f7a354a2f4319e8fd
Rifa Prasetya <rifaekaprasetya13@gmail.com>
2026-07-28 21:32:55 +0700

    feat: setup dan implementasi database

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import http from "http";
 import { Server } from "socket.io";
 import cors from "cors";
 import dotenv from "dotenv";
+import "./config/db.js";
 
 dotenv.config({ path: ".env.dev" });
 
